8755ae05dfb7aaa020faea564c9a073a3032b4d9
rajeev <rajeev.goonie98@gmail.com>
2020-03-25 10:58:20 -0700

    showing how to do 'or' conditional

--- 03-net-ninja-javascript-beginners-tutorial/tutorial-15/test.js
+++ 03-net-ninja-javascript-beginners-tutorial/tutorial-15/test.js
@@ -1,11 +1,11 @@
 var myAge = 25;
 
-if(myAge >= 18 && myAge <= 30){
+if(myAge <= 18 || myAge >= 30){
 
-    document.write("you can come you cool dude...");
+    document.write("you aint coming");
 
 }
 
 else {
-    document.write("you aint coming");
+    document.write("you can come you cool dude...");
 }
