46bc876b5131f03428e4e4cc286be852264ec7d1
rajeev <rajeev.goonie98@gmail.com>
2020-03-26 00:53:55 -0700

    creating required props for objects

--- 04-traversy-media-react-crash/todo-crash/src/components/Todos.js
+++ 04-traversy-media-react-crash/todo-crash/src/components/Todos.js
@@ -1,11 +1,18 @@
 import React from "react";
 import ToDoItem from './TodoItem'
+import PropTypes from 'prop-types';
 
 class Todos extends React.Component {
   render() {
     return this.props.todos.map((todo) => (
-        <ToDoItem todo={todo}/>
+        <ToDoItem key={todo.id} todo={todo}/>
     ));
   }
 }
+
+//Prop Types
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired
+}
+
 export default Todos;
