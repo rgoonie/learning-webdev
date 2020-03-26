9f6a1c72a2ae8149ce6e247f90b18b74521fbf80
rajeev <rajeev.goonie98@gmail.com>
2020-03-26 00:36:43 -0700

    formatted doc

--- 04-traversy-media-react-crash/todo-crash/src/components/Todos.js
+++ 04-traversy-media-react-crash/todo-crash/src/components/Todos.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
 class Todos extends React.Component {
-    render(){
-      return (
-          <div >
-            <h1>Todos</h1>
-          </div>
-      );
-    }
+  render() {
+    return (
+      <div>
+        <h1>Todos</h1>
+      </div>
+    );
+  }
 }
 export default Todos;
