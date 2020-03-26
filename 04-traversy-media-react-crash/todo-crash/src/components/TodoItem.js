accde38033e0283f4cb28036d6deaec6bafbcd86
rajeev <rajeev.goonie98@gmail.com>
2020-03-26 01:04:28 -0700

    adding all style for todo items

--- 04-traversy-media-react-crash/todo-crash/src/components/TodoItem.js
+++ 04-traversy-media-react-crash/todo-crash/src/components/TodoItem.js
@@ -4,16 +4,11 @@ import PropTypes from 'prop-types';
 export class TodoItem extends Component {
     
     getStyle = () => {
-        if(this.props.todo.completed){
-            return {
-                textDecoration: 'line-through'
-            }
-        }
-
-        else{
-            return {
-                textDecoration: 'none'
-            }
+        return {
+            background: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed? 'line-through' : 'none'
         }
     }
     
@@ -28,10 +23,6 @@ export class TodoItem extends Component {
 
 TodoItem.propTypes = {
     todo: PropTypes.object.isRequired
-}
-
-const itemStyle = {
-    backgroundColor: '#f4f4f4'
 }
 
 export default TodoItem
