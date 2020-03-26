240e583ff3fe46273486b1525eb706dc1150612f
rajeev <rajeev.goonie98@gmail.com>
2020-03-26 00:34:05 -0700

    created some todos as the states and pass them to the todos compontent as a prop

--- 04-traversy-media-react-crash/todo-crash/src/App.js
+++ 04-traversy-media-react-crash/todo-crash/src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import Todos from './components/Todos';
-import './App.css';
+import React from "react";
+import Todos from "./components/Todos";
+import "./App.css";
 
 class App extends React.Component {
-    render(){
-      return (
-          <div className="App">
-            <Todos />
-          </div>
-      );
-    }
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "take out the trash",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "take out the trash",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "take out the trash",
+        completed: false
+      }
+    ]
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos}/>
+      </div>
+    );
+  }
 }
 export default App;
