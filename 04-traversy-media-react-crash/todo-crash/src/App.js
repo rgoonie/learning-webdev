582d1f87ec7c0c93f6dd1a7c9b70fe790a638954
rajeev <rajeev.goonie98@gmail.com>
2020-03-26 01:21:25 -0700

    building the toggle feature

--- 04-traversy-media-react-crash/todo-crash/src/App.js
+++ 04-traversy-media-react-crash/todo-crash/src/App.js
@@ -23,6 +23,7 @@ class App extends React.Component {
     ]
   };
 
+  // Toggle Complete
   markComplete = (id) => {
     this.setState({ todos: this.state.todos.map(todo => {
       if(todo.id === id){
